cfcbf0db23c7ad94c2376c5c67556d95aa43f4a1
admin <password>
2022-02-05 15:30:13 +0000

    working on maps

--- prototype/ios/Runner/AppDelegate.swift
+++ prototype/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAlb2y4PT8shplcLijTaObFr7VwY59U1wo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- prototype/lib/screens/authenticate/sign_in.dart
+++ prototype/lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prototype/screens/maps/maps.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -14,7 +15,15 @@ class _SignInState extends State<SignIn> {
       backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
-          Text('Log In!')
+          Text('Log In!'),
+          IconButton(
+              icon: Icon(
+                Icons.call,
+                size: 20.0,
+                color: Colors.brown[900],
+              ),
+              onPressed: () => Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => Map()))),
         ],
       ),
     );
